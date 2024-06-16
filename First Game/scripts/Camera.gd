5ffa03c1bcefde0ea51c0aba78253d75b80ebf68
YeetmyFeet <yeetmyfeet69420bruh@gmail.com>
2024-06-16 01:15:47 -0700

    Basic scene and screen wrapping

---
+++ First Game/scripts/Camera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+@onready var ball = $"../RigidBody2D"
+@onready var max_height = ball.position.y
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var ball_y = ball.position.y
+	
+	if ball_y > position.y + 330:
+		pass # fell off
+		
+	if ball_y < max_height:
+		max_height = ball_y
+	position.y = roundf(max_height)
--- First Game/scripts/ball.gd
+++ First Game/scripts/ball.gd
@@ -3,7 +3,9 @@ extends RigidBody2D
 
 @export var force = 5
 @export var max_line_length = 300
+const wrap_limit = 430
 var dragging
+var added_child
 
 var line = Line2D.new()
 var line_vector = Vector2()
@@ -20,20 +22,22 @@ func _ready():
 	# Line
 	line.z_index = -1
 	line.end_cap_mode = Line2D.LINE_CAP_ROUND
-	line.gradient = gradient
+	#line.gradient = gradient
 	line.width_curve = curve
 
 func _process(delta):
 	if Input.is_action_pressed("click"):
 		line_vector = (get_global_mouse_position() - global_position).normalized() \
 				* minf((get_global_mouse_position() - global_position).length(), max_line_length)
-		#line.default_color = gradient.sample(1 - line_vector.length() / max_line_length)
+		line.default_color = gradient.sample(1 - line_vector.length() / max_line_length)
 		line.global_position = global_position
 		line.set_point_position(1, line_vector)
 
 func _input(event):
 	if event.is_action_pressed("click") and not dragging:
-		$"..".add_child((line))
+		if !added_child:
+			added_child = true
+			$"..".add_child((line))
 		line.add_point(Vector2())
 		line.add_point(Vector2(), 1)
 		dragging = true
@@ -44,3 +48,9 @@ func _input(event):
 		linear_velocity = line_vector * force
 		dragging = false
 		Engine.time_scale = 1
+		
+func _integrate_forces(state):
+	if position.x < wrap_limit * -1:
+		position.x = wrap_limit
+	if position.x > wrap_limit:
+		position.x = wrap_limit * -1
