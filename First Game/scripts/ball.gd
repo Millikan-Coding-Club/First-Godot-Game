029f0aa6674c79d990699435f70002e929ec0e12
YeetmyFeet <yeetmyfeet69420bruh@gmail.com>
2024-06-15 17:44:51 -0700

    Initialized repo

---
+++ First Game/scripts/ball.gd
@@ -0,0 +1,46 @@
+extends RigidBody2D
+
+
+@export var force = 5
+@export var max_line_length = 300
+var dragging
+
+var line = Line2D.new()
+var line_vector = Vector2()
+var gradient = Gradient.new()
+var curve = Curve.new()
+
+func _ready():
+	# Curve
+	curve.add_point(Vector2(0, 2.5))
+	curve.add_point(Vector2(1, 1))
+	# Gradient
+	gradient.set_color(0, Color.RED)
+	gradient.set_color(1, Color.YELLOW)
+	# Line
+	line.z_index = -1
+	line.end_cap_mode = Line2D.LINE_CAP_ROUND
+	line.gradient = gradient
+	line.width_curve = curve
+
+func _process(delta):
+	if Input.is_action_pressed("click"):
+		line_vector = (get_global_mouse_position() - global_position).normalized() \
+				* minf((get_global_mouse_position() - global_position).length(), max_line_length)
+		#line.default_color = gradient.sample(1 - line_vector.length() / max_line_length)
+		line.global_position = global_position
+		line.set_point_position(1, line_vector)
+
+func _input(event):
+	if event.is_action_pressed("click") and not dragging:
+		$"..".add_child((line))
+		line.add_point(Vector2())
+		line.add_point(Vector2(), 1)
+		dragging = true
+		Engine.time_scale = 0.25
+		
+	if event.is_action_released("click") and dragging:
+		line.clear_points()
+		linear_velocity = line_vector * force
+		dragging = false
+		Engine.time_scale = 1
